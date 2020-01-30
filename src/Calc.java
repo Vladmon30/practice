0c3d6b38da28b10d1fe8a851f2d784d7b4cac1e1
Vlad <vladmonqa35@gmail.com>
2020-01-30 16:51:11 -0600

    adding 2 new classes

---
+++ src/Calc.java
@@ -0,0 +1,7 @@
+public class Calc {
+    public static void main(String[] args) {
+        int num2 =1;
+        int num1 = 3;
+        Calculator.add(num1, num2);
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public static int add(int a, int b){
+        int res = a+b;
+        return res;
+    }
+
+
+
+}
