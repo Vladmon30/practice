7812c6b21a64d9aeb578b098aca439ec681ab2f7
Vladmon30 <44418882+Vladmon30@users.noreply.github.com>
2020-01-31 21:59:44 -0600

    add couple int +  string gas

--- src/Calc.java
+++ src/Calc.java
@@ -2,6 +2,9 @@ public class Calc {
     public static void main(String[] args) {
         int num2 =1;
         int num1 = 3;
+        String gas = "disel"; 
+        int num3;
+        int num4;
         Calculator.add(num1, num2);
         Calculator.substract(num1,num2);
         System.out.println(Calculator.add(num1, num2));
