5211179f6ee8ada84eb99955fc9e06abc14ad335
Vlad <vladmonqa35@gmail.com>
2020-02-01 16:21:41 -0600

    add devide method to Calc Class

--- src/Calc.java
+++ src/Calc.java
@@ -1,22 +1,25 @@
 public class Calc {
     public static void main(String[] args) {
-        int num2 =1;
+        int num2 = 1;
         int num1 = 3;
 
 // its very cool when you know what to do
         String Name = "MySonName";
         int sonAge = 10;
-        int daughterAge =5;
-        String gas = "disel"; 
+        int daughterAge = 5;
+        String gas = "disel";
         int num3;
         int num4;
 
         Calculator.add(num1, num2);
         // call substract method from calculator class
-        Calculator.substract(num1,num2);
+        Calculator.substract(num1, num2);
         System.out.println(Calculator.add(num1, num2));
-        System.out.println(Calculator.substract(num1,num2));
+        System.out.println(Calculator.substract(num1, num2));
         // adding comments to Calc class
         // its a good practice
     }
+        public static int devide( int a, int b){
+            return a/b;
+        }
 }
