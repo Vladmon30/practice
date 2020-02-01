12eb2fa388451c7721922b7d2b5718e294f211dc
Vlad <vladmonqa35@gmail.com>
2020-02-01 16:36:26 -0600

    add local comment

--- src/PrintingMessage.java
+++ src/PrintingMessage.java
@@ -1,9 +1,9 @@
 public class PrintingMessage {
 
     public static void main(String[] args) {
-
+// Local changes 1
         System.out.println("Java is Fun");
-
+// Local changes 2
         for (int i = 1; i <= 5; i++) {
             System.out.println("But not always");
         }
