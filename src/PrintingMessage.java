877ec1b085d26d81067b09e432a0190fc2d124e0
Vladmon30 <44418882+Vladmon30@users.noreply.github.com>
2020-02-01 16:33:52 -0600

    Update PrintingMessage.java

--- src/PrintingMessage.java
+++ src/PrintingMessage.java
@@ -1,9 +1,9 @@
 public class PrintingMessage {
 
     public static void main(String[] args) {
-
+//GitHub changes 1
         System.out.println("Java is Fun");
-
+//GitHub changes 2
         for (int i = 1; i <= 5; i++) {
             System.out.println("But not always");
         }
