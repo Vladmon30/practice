f40eb52cac2377e4ccd890a20b3341a0260deeef
Vladmon30 <44418882+Vladmon30@users.noreply.github.com>
2020-02-01 12:07:26 -0600

    change parameters

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,14 +1,14 @@
 public class Calculator {
 //Training Git/GitHub
     //and I feel goooood!!!
-    public static int add(int a, int b){
-        int res = a+b;
+    public static int add(int i, int j){
+        int res = i+j;
         return res;
     }
 // add new subtract method
 //also its suppose to substract to integer numbers    
-    public static  int substract(int d, int c){
-        return d-c;
+    public static  int substract(int a, int b){
+        return a-b;
     }
  //adding new multiply method     
     public static  int multiply(int a, int b){
@@ -16,7 +16,7 @@ public class Calculator {
     }
 
  // add devide method
-    public static int devide( int a, int b){ return a/b; }
+    public static int devide( int c, int b){ return c/b; }
 
  //add new devide% method    
   public static int devideWithProcent (int d, int c){return d%c;}
