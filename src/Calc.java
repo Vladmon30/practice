9154727444a868a59ad6065030d1dc225c5b2859
Vlad <vladmonqa35@gmail.com>
2020-01-31 22:03:59 -0600

    add a lot of new stuff

--- src/Calc.java
+++ src/Calc.java
@@ -2,6 +2,11 @@ public class Calc {
     public static void main(String[] args) {
         int num2 =1;
         int num1 = 3;
+// its very cool when you know what to do
+        String Name = "MySonName";
+        int sonAge = 10;
+        int daughterAge =5;
+
         Calculator.add(num1, num2);
         Calculator.substract(num1,num2);
         System.out.println(Calculator.add(num1, num2));
