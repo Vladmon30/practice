f1ad7829a35bb389adfc198527b0ef37fd7ddce9
Vlad <vladmonqa35@gmail.com>
2020-02-01 15:51:05 -0600

    clean class Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,17 @@
 public class Calculator {
-//Training Git/GitHub
-    //and I feel goooood!!!
+
     public static int add(int i, int j){
         int res = i+j;
         return res;
     }
- //adding new multiply method
- // changed some parameters
+
     public static  int multiply(int a, int b){
         int count = a*b;
         return count;
     }
 
- // add devide method
     public static int devide( int c, int b){ return c/b; }
 
- // Its always fun to do something good in life and help other people for free!!!
 
 }
 
