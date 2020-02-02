a3e803051ededaba99b285f5970c8c9650ed0f2c
Vladmon30 <44418882+Vladmon30@users.noreply.github.com>
2020-02-02 12:24:52 -0600

    delete comments

--- src/Calc.java
+++ src/Calc.java
@@ -2,8 +2,7 @@ public class Calc {
     public static void main(String[] args) {
         int num2 = 1;
         int num1 = 3;
-
-// its very cool when you know what to do
+        
         String Name = "MySonName";
         int sonAge = 10;
         int daughterAge = 5;
@@ -12,13 +11,7 @@ public class Calc {
         int num4;
 
         Calculator.add(num1, num2);
-        // call substract method from calculator class
-
         System.out.println(Calculator.add(num1, num2));
-
-        // adding comments to Calc class
-        // its a good practice
-
     }
         public static int devide ( int a, int b){
             return a / b;
