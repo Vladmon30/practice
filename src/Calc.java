f7beaf3d189cc468171b063b6421d19051134fcf
Vlad <vladmonqa35@gmail.com>
2020-02-02 12:27:07 -0600

    add some changes

--- src/Calc.java
+++ src/Calc.java
@@ -2,7 +2,6 @@ public class Calc {
     public static void main(String[] args) {
         int num2 = 1;
         int num1 = 3;
-
 // its very cool when you know what to do
         String Name = "MySonName";
         int sonAge = 10;
@@ -10,15 +9,10 @@ public class Calc {
         String gas = "disel";
         int num3;
         int num4;
-
         Calculator.add(num1, num2);
-        // call substract method from calculator class
-
-        System.out.println(Calculator.add(num1, num2));
-
-        // adding comments to Calc class
+// call substract method from calculator class
+        System.out.println(Calculator.add(num1, num2)); // adding comments to Calc class
         // its a good practice
-
     }
         public static int devide ( int a, int b){
             return a / b;
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,12 @@
 public class Calculator {
-
     public static int add(int a, int b){
         int res = a+b;
         return res;
     }
-
     public static  int multiply(int a, int b){
         int res = a * b;
         return res;
     }
-
     public static int devide( int a, int b){
         return a/b; 
     }
